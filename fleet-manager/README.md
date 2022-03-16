ed4217ce38a61a7ae25b1792cde71b2fdd818236
Ulf Lilleengen <ulf.lilleengen@gmail.com>
2022-03-16 07:27:27 +0100

    Start consuming events from websocket

---
+++ fleet-manager/README.md
@@ -0,0 +1,3 @@
+# fleet-manager
+
+The Drogue IoT fleet manager is a addon to Drogue IoT that allows you to build and manage firmware for a fleet of devices on Kubernetes. The fleet manager integrates with the Drogue IoT Cloud to communicate with devices that have firmware update capability, and manages devices for a single application.
--- fleet-manager/src/main.rs
+++ fleet-manager/src/main.rs
@@ -1,5 +1,6 @@
 use actix_web::{get, web, App, HttpResponse, HttpServer, Responder};
-use anyhow::anyhow;
+use anyhow::{anyhow, Context};
+use clap::{Parser, Subcommand};
 use oci_distribution::{client, secrets::RegistryAuth};
 use serde::{Deserialize, Serialize};
 use serde_json::Value;
@@ -8,6 +9,8 @@ use std::path::PathBuf;
 use std::str::FromStr;
 use std::sync::Arc;
 use std::sync::Mutex;
+use tokio_tungstenite::connect_async;
+use tungstenite::http::Request;
 
 #[derive(Serialize, Deserialize)]
 pub struct PollResponse {
@@ -170,8 +173,32 @@ impl Index {
     }
 }
 
+#[derive(Parser, Debug)]
+struct Args {
+    /// Directory where firmware index is stored
+    #[clap(short, long, default_value = "/registry")]
+    index_dir: PathBuf,
+
+    /// Prefix to use for container registry storing images
+    #[clap(short, long)]
+    registry_prefix: String,
+
+    /// URL to websocket endpoint for application
+    #[clap(short, long)]
+    url: String,
+
+    /// Token for authenticating fleet manager to Drogue IoT
+    #[clap(short, long)]
+    token: String,
+
+    /// Username for authenticating fleet manager to Drogue IoT
+    #[clap(short, long)]
+    user: String,
+}
+
 #[actix_web::main]
-async fn main() -> std::io::Result<()> {
+async fn main() -> anyhow::Result<()> {
+    let args = Args::parse();
     env_logger::init();
 
     let client = Arc::new(Mutex::new(client::Client::new(client::ClientConfig {
@@ -180,23 +207,43 @@ async fn main() -> std::io::Result<()> {
         accept_invalid_certificates: true,
         extra_root_certificates: Vec::new(),
     })));
-    let index_dir = std::env::var("INDEX_DIR").unwrap_or("/registry/".to_string());
-    let index = Index::new(PathBuf::from_str(&index_dir).unwrap());
-    let prefix = std::env::var("REGISTRY_PREFIX").unwrap();
-    let token = std::env::var("REGISTRY_TOKEN").unwrap();
+    let index_dir = args.index_dir;
+    let index = Index::new(index_dir);
+    let url = args.url;
+    let token = args.token;
+    let user = args.user;
+
+    let encoded = base64::encode(&format!("{}:{}", user, token).as_bytes());
+    let basic_header = format!("Basic {}", encoded);
+
+    let request = Request::builder()
+        .uri(url)
+        .header(tungstenite::http::header::AUTHORIZATION, basic_header)
+        .body(())
+        .context("Error building websocket request")?;
+
+    log::debug!("Connecting to websocket with request : {:?}", request);
+    let (mut socket, response) = connect_async(request)
+        .await
+        .context("Error connecting to the websocket endpoint:")?;
+    log::debug!("HTTP response: {}", response.status());
+
+    //let prefix = arstd::env::var("REGISTRY_PREFIX").unwrap();
+    //let token = std::env::var("REGISTRY_TOKEN").unwrap();
     HttpServer::new(move || {
         App::new()
-            .app_data(web::Data::new(OciClient {
+            /*     .app_data(web::Data::new(OciClient {
                 client: client.clone(),
                 token: token.clone(),
                 prefix: prefix.clone(),
             }))
             .app_data(web::Data::new(index.clone()))
             .route("/v1/poll/{image}", web::get().to(poll))
-            .route("/v1/fetch/{image}/{version}", web::get().to(fetch))
+            .route("/v1/fetch/{image}/{version}", web::get().to(fetch))*/
             .route("/healthz", web::get().to(healthz))
     })
     .bind(("0.0.0.0", 8080))?
     .run()
-    .await
+    .await?;
+    Ok(())
 }
