9a2fed797bca05c2d6411f06365eb22156b8ca53
Ulf Lilleengen <lulf@redhat.com>
2022-03-25 21:56:52 +0100

    Add metadata output

---
+++ builder/scripts/build.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+set -e
+
+PROJECT=$1
+CARGO_BUILD_ARGS=$2
+
+pushd ${PROJECT}
+REVISION=$(git rev-parse --short HEAD | tr -d '\n')
+echo "Building firmware"
+cargo build --release ${CARGO_BUILD_ARGS}
+
+echo "Creating binary file"
+cargo objcopy --release ${CARGO_BUILD_ARGS} -- -O binary firmware.bin
+SZ=$(du -b firmware.bin | cut -f1)
+CHECKSUM=$(sha256sum firmware.bin)
+
+cat<<EOF > firmware.json
+{
+  "version": "${REVISION}",
+  "size": ${SZ},
+  "checksum": "${CHECKSUM}"
+}
+EOF
+
+echo "Generated metadata:"
+cat firmware.json
+popd
