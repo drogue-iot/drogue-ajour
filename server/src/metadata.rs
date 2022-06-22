33e9743100e5651c0bfb671e0c5df6a36079bd5f
Ulf Lilleengen <lulf@redhat.com>
2022-06-22 13:38:28 +0200

    Use serde_bytes for metadata version

--- server/src/metadata.rs
+++ server/src/metadata.rs
@@ -2,6 +2,7 @@ use serde::{Deserialize, Serialize};
 
 #[derive(Serialize, Deserialize, Debug, Clone)]
 pub struct Metadata {
+    #[serde(with = "serde_bytes")]
     pub version: Vec<u8>,
     pub checksum: String,
     pub size: u32,
