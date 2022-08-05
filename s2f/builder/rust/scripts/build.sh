f921450fbaf53e4f40709f85752f3d9bba24402d
Ulf Lilleengen <lulf@redhat.com>
2022-08-05 11:10:27 +0200

    Use context dir

--- s2f/builder/rust/scripts/build.sh
+++ s2f/builder/rust/scripts/build.sh
@@ -2,15 +2,9 @@
 set -x
 set -e
 
-PROJECT=$1
-
-pushd ${PROJECT}
-
 REVISION=$(git rev-parse --short HEAD | tr -d '\n')
 echo "Building firmware"
 REVISION=${REVISION} cargo build --release ${BUILD_ARGS}
 
 echo "Creating binary file"
 REVISION=${REVISION} cargo objcopy --release ${BUILD_ARGS} -- -O binary artifact.bin
-
-popd
