6d8171d18c32c8b4e6698961d6cc4d9d300fb536
Ulf Lilleengen <lulf@redhat.com>
2022-08-08 14:15:39 +0200

    Pass build args as expected

--- s2f/builder/rust/scripts/build.sh
+++ s2f/builder/rust/scripts/build.sh
@@ -4,7 +4,7 @@ set -e
 
 REVISION=$(git rev-parse --short HEAD | tr -d '\n')
 echo "Building firmware"
-REVISION=${REVISION} cargo build --release ${BUILD_ARGS}
+REVISION=${REVISION} cargo build --release $@
 
 echo "Creating binary file"
-REVISION=${REVISION} cargo objcopy --release ${BUILD_ARGS} -- -O binary firmware.bin
+REVISION=${REVISION} cargo objcopy --release $@ -- -O binary firmware.bin
